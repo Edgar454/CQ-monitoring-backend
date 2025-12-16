c895d5f6bc2b30e77bc307ed5a41716148f49be2
Edgar Mevaa <mevaed4@gmail.com>
2025-12-16 09:31:35 +0100

    fix: fixed a bug that maide the call fail if the status wasn't the exact same by relaxing the condition to accept lowercase versions

--- mock_server.py
+++ mock_server.py
@@ -43,7 +43,9 @@ def get_tests(
             "duration_sec": 2700,
             "return": round(random.uniform(-0.1, 0.1), 4),
             "link_status": "OK",
-            "state": state if state in VALID_STATES else random.choice(VALID_STATES)
+            "state": state if 
+                     state is not None and state.upper() in VALID_STATES
+                     else random.choice(VALID_STATES)
         }
         for _ in range(3)
     ]
@@ -125,7 +127,9 @@ def get_performance(
             "latency_ms_p95": random.randint(50, 200),
             "cpu_usage": round(random.uniform(0, 1), 2),
             "memory_usage_mb": random.randint(1000, 4000),
-            "status": status if status in VALID_STATUS else random.choice(VALID_STATUS),
+            "status": status if 
+                    status is not None and status.upper() in VALID_STATUS 
+                    else random.choice(VALID_STATUS),
             "errors": random.randint(0, 5)
         }
         for i in range(min(limit, 10))
@@ -156,7 +160,9 @@ def get_alerts(
             "metric": "latency_ms_p95",
             "value": random.randint(150, 300),
             "threshold": 150,
-            "status": status if status in VALID_STATUS else random.choice(VALID_STATUS)
+            "status": status 
+                if status is not None and status.upper() in VALID_STATUS 
+                else random.choice(VALID_STATUS)
         }
         for i in range(3)
     ]
